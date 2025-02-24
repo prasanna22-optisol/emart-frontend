ccef0c62a5ad503a96b2cbcfb1eda33042aba098
prasanna22-optisol <prasannakumar.m@optisolbusiness.com>
2025-02-24 22:09:05 +0530

    commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CustomerService } from '../../services/customer.service';
 import { Product } from '../../../types/product';
 import { APIResponse } from '../../../types/api_response';
-import { MatButton } from '@angular/material/button';
 import { ProductCardComponent } from "../product-card/product-card.component";
 import {CarouselModule, OwlOptions} from "ngx-owl-carousel-o"
 @Component({
@@ -28,17 +27,18 @@ export class HomeComponent implements OnInit {
   customerService=inject(CustomerService)
   newProducts:Product[]=[]
   featuredProducts:Product[]=[]
-
-
+  bannerImages:Product[]=[]
 
   ngOnInit(): void {
     this.customerService.getNewProducts().subscribe((res:APIResponse<Product[]>)=>{
       this.newProducts=res.data
       console.log(this.newProducts)
+      this.bannerImages.push(...res.data)
     })
     this.customerService.getFeaturedProducts().subscribe((res:APIResponse<Product[]>)=>{
       this.featuredProducts=res.data
       console.log(this.featuredProducts)
+      this.bannerImages.push(...res.data)
     })
   }
 
