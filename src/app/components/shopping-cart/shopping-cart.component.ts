cee38ac050d5bdc7fce12da1a79f955b8aeca6d2
prasanna22-optisol <prasannakumar.m@optisolbusiness.com>
2025-02-28 05:35:21 +0530

    commit

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../../types/product';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-cart.component.scss'
 })
 export class ShoppingCartComponent {
+
+
+  cartService = inject(CartService)
+
+  ngOnInit(){
+    this.cartService.init()
+  }
+
+  sellingPrice(product:Product){
+    return product.price
+  }
+
+  get cartItems() {
+    return this.cartService.items;
+  }
+
+  get totalAmount() {
+    let amount = 0;
+    for (let index = 0; index < this.cartItems.length; index++) {
+      const element = this.cartItems[index];
+      amount += this.sellingPrice(element.product) * element.quantity;
+    }
+    return amount;
+  }
+
+  addToCart(productId:string,quantity:number){
+    this.cartService.addToCart(productId,quantity).subscribe((result)=>{
+      this.cartService.init()
+    })
+  }
+
+  checkout() {
+  }
 
 }
