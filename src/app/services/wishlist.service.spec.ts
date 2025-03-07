aae523f4e7e65d8e208903a59196fe086c74d1ef
prasanna22-optisol <prasannakumar.m@optisolbusiness.com>
2025-03-07 13:59:32 +0530

    commit

--- src/app/services/wishlist.service.spec.ts
+++ src/app/services/wishlist.service.spec.ts
@@ -1,16 +1,86 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WishlistService } from './wishlist.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { APIResponse } from '../../types/api_response';
+import { WishList } from '../../types/wishlist';
 
-describe('WishlistService', () => {
+fdescribe('WishlistService', () => {
   let service: WishlistService;
+  let httpMock:HttpTestingController
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers:[
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(WishlistService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  afterEach(()=>{
+    httpMock.verify()
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch wishlists',()=>{
+    const mockResponse:APIResponse<WishList[]>={
+      message:'Wishlists fetched successfully',
+      statusCode:200,
+      data:[
+        {
+          _id:'1',
+          userId:'123',
+          productId:'456'
+        }
+      ]
+    };
+
+    service.getWishList().subscribe((response)=>{
+      expect(response.data).toEqual(mockResponse.data);
+    });
+
+    const req=httpMock.expectOne('http://localhost:3000/api/home/wishlists')
+    expect(req.request.method).toBe('GET')
+    req.flush(mockResponse)
+  });
+
+  it('should add product to wishlist',()=>{
+    const productId = '456';
+    const mockResponse: APIResponse<WishList> = {
+      message: 'Added to wishlist',
+      statusCode: 201,
+      data: { _id: '1', userId: '123', productId: '456' }
+    };
+    service.addWishLists(productId).subscribe((response)=>{
+      expect(response.data).toEqual(response.data)
+    })
+    const req = httpMock.expectOne(`http://localhost:3000/api/home/add-to-wishlist?productId=${productId}`);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockResponse);
+  })
+  it('should remove a product from wishlist', () => {
+    const productId = '456';
+    const mockResponse: APIResponse<WishList> = {
+      message: 'Removed from wishlist',
+      statusCode: 200,
+      data: { _id: '1', userId: '123', productId: '456' }
+    };
+
+    service.removeWishlist(productId).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`http://localhost:3000/api/home/remove-from-wishlist/${productId}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(mockResponse);
+  });
+
+
 });
